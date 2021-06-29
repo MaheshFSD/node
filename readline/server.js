406777e1c5757d2532d11654d8058fab796a62d6
MaheshFSD <maheshfsdeveloper@gmail.com>
2021-06-29 17:03:44 +0530

    creating a listener on readline

--- readline/server.js
+++ readline/server.js
@@ -11,3 +11,9 @@ readline1.question("What is your name", function(name){
         readline1.close()
     })
 })
+
+//This readline is inherited from events, so it can use on method.
+readline1.on("close",function(){
+    console.log("bye bye")
+    process.exit(0)
+})
